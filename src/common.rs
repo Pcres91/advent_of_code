6cd7c5673cbe02960c74e0afc284f73a0d7a5ec3
Paul-Mac <paulcresswell@live.com>
2024-12-11 13:44:06 +0100

    day one complete

--- src/common.rs
+++ src/common.rs
@@ -1,5 +1,54 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::panic;
+use std::path::Path;
 
-// pub fn open_file(path: &Path) -> io::Result<File> {
-//     println!("entered open_file {path:?}");
-//     File::open(path)
-// }
+pub fn day1() {
+    println!("entered day 1");
+
+    let input_location = Path::new("input/1.txt");
+
+    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where
+        P: AsRef<Path>,
+    {
+        let file = File::open(filename)?;
+        Ok(io::BufReader::new(file).lines())
+    }
+
+    let mut lhs = vec![];
+    let mut rhs = vec![];
+    if let Ok(lines) = read_lines(input_location) {
+        for line in lines.flatten() {
+            // println!("{line}");
+            let inputs: Vec<u32> = line
+                .split_whitespace()
+                .map(|x| x.parse().expect("Not an integer!"))
+                .collect();
+            let len = inputs.len();
+            if len != 2 {
+                panic!("input was not the right size. Len: {len}");
+            }
+            lhs.push(inputs[0]);
+            rhs.push(inputs[1]);
+        }
+    } else {
+        panic!("Failed to open input file {input_location:?}");
+    }
+
+    lhs.sort();
+    rhs.sort();
+
+    let result = lhs
+        .iter()
+        .zip(rhs.clone())
+        .fold(0, |acc, (lhs, rhs)| acc + (lhs).abs_diff(rhs));
+
+    println!("Part one: {result}");
+
+    let part_2_result = lhs.iter().fold(0, |acc, val| {
+            acc + (val * rhs.iter().filter(|x| *x == val).count() as u32)
+    });
+
+    println!("Part two: {part_2_result}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,5 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::panic;
-use std::path::Path;
-
 mod common;
 
-fn day1() {
-    println!("entered day 1");
-
-    let input_location = Path::new("input/1.txt");
-
-    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where
-        P: AsRef<Path>,
-    {
-        let file = File::open(filename)?;
-        Ok(io::BufReader::new(file).lines())
-    }
-
-    let mut lhs = vec![];
-    let mut rhs = vec![];
-    if let Ok(lines) = read_lines(input_location) {
-        for line in lines.flatten() {
-            // println!("{line}");
-            let inputs: Vec<u32> = line
-                .split_whitespace()
-                .map(|x| x.parse().expect("Not an integer!"))
-                .collect();
-            let len = inputs.len();
-            if (len != 2) {
-                panic!("input was not the right size. Len: {len}");
-            }
-            lhs.push(inputs[0]);
-            rhs.push(inputs[1]);
-        }
-    } else {
-        panic!("Failed to open input file {input_location:?}");
-    }
-
-    lhs.sort();
-    rhs.sort();
-
-    let result = lhs
-        .iter()
-        .zip(rhs)
-        .fold(0, |acc, (lhs, rhs)| acc + (lhs).abs_diff(rhs));
-
-    println!("Part one: {result}")
-}
-
 fn main() {
-    day1()
+    common::day1()
 }
